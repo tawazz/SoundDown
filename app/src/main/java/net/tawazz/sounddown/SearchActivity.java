893ba2b0060c4113596eb9ded375967b293745c3
tawazz <tawanda.nyakudjga@gmail.com>
2015-09-23 20:21:48 +0800

    init

---
+++ app/src/main/java/net/tawazz/sounddown/SearchActivity.java
@@ -0,0 +1,51 @@
+package net.tawazz.sounddown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private Button searchBtn;
+    private EditText searchField;
+    private ListView songs;
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        view = getWindow().getDecorView().findViewById(android.R.id.content);
+        searchBtn = (Button) view.findViewById(R.id.button_search);
+        searchField =(EditText) view.findViewById(R.id.editText_search);
+        songs = (ListView) view.findViewById(R.id.listView_songs);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
